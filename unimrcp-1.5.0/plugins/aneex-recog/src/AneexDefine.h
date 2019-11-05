4f4b296cc868a098387d7b681239504711f875b3
Anastasya83 <vice83@mail.ru>
2019-11-05 17:02:25 +0300

    Create AneexDefine.h

---
+++ unimrcp-1.5.0/plugins/aneex-recog/src/AneexDefine.h
@@ -0,0 +1,17 @@
+#ifndef ANEEXDEFINE_H
+#define ANEEXDEFINE_H
+
+// This namespace contains name of functions, using in library
+// for UniMRCP project
+
+class AudioIndexingTask
+{
+public:
+/* ============================ CREATORS =================================== */
+	AudioIndexingTask();
+	~AudioIndexingTask();
+protected:
+private:
+};
+
+#endif // ANEEXDEFINE_H
