4fe9f71e909ae43a242c5c3c238020af8b1f50e8
Anastasya83 <vice83@mail.ru>
2019-11-12 16:24:40 +0300

    chg

--- unimrcp-1.5.0/plugins/aneex-recog/src/aneex_recog_engine.c
+++ unimrcp-1.5.0/plugins/aneex-recog/src/aneex_recog_engine.c
@@ -597,7 +597,8 @@ static apt_bool_t aneex_recog_stream_write(mpf_audio_stream_t *stream, const mpf
 			fwrite(frame->codec_frame.buffer,1,frame->codec_frame.size,recog_channel->audio_out);
 
 			//printf("Size buffer=%d\n", frame->codec_frame.size);
-			aneex_recog_from_db(audio_file_path, db_file_path, recog_channel);
+			//aneex_recog_from_db(audio_file_path, db_file_path, recog_channel);
+			aneex_recog_from_db("/usr/local/unimrcp/data/Etalons/avto 02.wav", db_file_path, recog_channel);
 		}
 
 	}
